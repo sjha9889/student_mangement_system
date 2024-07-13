779d4fbbfa0788bafef20e4344812f1303cf6cd4
shubham <2105834@gmail.com>
2024-07-13 14:13:28 +0530

    done

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dc^@$hbz9tra2w#p0k_8o5hd3(hv#7kv+^#5&$^!_hp!19fgm9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
